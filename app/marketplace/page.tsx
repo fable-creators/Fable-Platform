05fe30e80bdd7a10bcf6e8b1e9c1988d206eaebc
Fable <156545331+fable-creators@users.noreply.github.com>
2024-10-03 01:19:54 +0000

    added basic layout for marketplace

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,13 +1,5 @@
+import MarketplacePage from '../components/MarketplacePage'
 
-export default function MarketplacePage() {
-  return (
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-4 text-coffee dark:text-sky">
-          Marketplace
-        </h1>
-        <p className="text-grape dark:text-sand">
-          Content for the Marketplace page will be added here.
-        </p>
-      </div>
-  );
+export default function Marketplace() {
+  return <MarketplacePage />
 }
